55e08057a86a2cadf3dd886b0ea53dff70796b28
0xfffb <jailbreak@aliyun.com>
2025-12-03 16:38:04 +0800

    feat: add demo

--- src/compiler/compiler.js
+++ src/compiler/compiler.js
@@ -6,6 +6,7 @@ class Compiler {
     constructor(source, opcode) {
         this.ast = this.parse(source)
         this.opcode = opcode
+        this.dependencies = ['console']
     }
 
     parse(source) {
@@ -29,6 +30,14 @@ class Compiler {
             },
             Identifier: {
                 enter: (path) => {
+                    if (this.dependencies.includes(path.node.name)) {
+                        console.log(`🤖 Dependency found: ${path.node.name}`)
+                        return
+                    }
+                    if (path.parent.type === 'MemberExpression' && path.parent.property === path.node && !path.parent.computed) {
+                        console.log(`🤖 String for dependency found: ${path.node.name}`)
+                        return
+                    }
                     console.log(path.node.name)
                 }
             },
