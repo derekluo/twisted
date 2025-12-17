d6663b8472a4f6770104e9b1941776b7b0e01f59
0xfffb <jailbreak@aliyun.com>
2025-12-18 00:19:02 +0800

    feat: add statement

--- src/compiler/compiler.ts
+++ src/compiler/compiler.ts
@@ -14,12 +14,14 @@ import {
 	ThrowStatement,
 	MemberExpression,
 	Expression,
+	Program,
+	Statement,
 } from "@babel/types";
 import { Opcode } from "../constant.js";
 import { ArgKind, createInstruction, type Instruction } from "../instruction.js";
 
 class Compiler {
-	private ast: ParseResult;
+	private program: Program;
 	public ir: Instruction[];
 	private globals: Map<string, number>;
 	private globalIndex: number;
@@ -27,7 +29,7 @@ class Compiler {
 	private consoleDependencyMethods: string[];
 
 	constructor(source: string) {
-		this.ast = parser.parse(source, { sourceType: "module" });
+		this.program = parser.parse(source, { sourceType: "module" }).program;
 		this.ir = [];
 		this.globals = new Map();
 		this.globalIndex = 0;
@@ -36,95 +38,24 @@ class Compiler {
 	}
 
 	compile(): Instruction[] {
-		traverse.default(this.ast, this.visitor());
+		if (!this.program) {
+			throw new Error("Failed to parse program");
+		}
+		this.program.body.forEach(element => {
+			this.compileStatement(element);
+		});
 		console.log("🤖 Compiled intermediate representation.");
 		return this.ir;
 	}
 
-	private visitor(): Visitor {
-		return {
-			Function: {
-				exit: (path: NodePath<Function>) => {},
-			},
-			CallExpression: {
-				exit: (path: NodePath<CallExpression>) => {
-					console.log("🤖 CallExpression length: ", path.node.arguments.length);
-					console.log("🤖 CallExpression callee: ", path.node.callee.type);
-				}
-			},
-			LogicalExpression: {
-				exit: (path: NodePath<LogicalExpression>) => {
-					console.log("🤖 LogicalExpression: ", path.node.operator);
-				}
-			},
-			ThrowStatement: {
-				exit: (path: NodePath<ThrowStatement>) => {
-					console.log("🤖 ThrowStatement: ", path.node.argument.type);
-				}
-			},
-			VariableDeclarator: {
-				exit: (path: NodePath<VariableDeclarator>) => {
-					const varName = (path.node.id as Identifier).name;
-					const binding = path.scope.getBinding(varName);
-					if (!binding) return;
-					switch (path.parent.type) {
-						case "VariableDeclarator":
-							console.log("🤖 VariableDeclarator parent type: %s, value: %s", path.parent.type, varName);
-							break;
-						default:
-							console.log("🤖 VariableDeclarator parent type: %s, value: %s", path.parent.type, varName);
-							break;
-					}
-				},
-			},
-			Identifier: {
-				exit: (path: NodePath<Identifier>) => {
-					if (!path.isReferencedIdentifier()) return;
-					if (this.dependencies.includes(path.node.name)) {
-						console.log("🤖 Identifier %s is a dependency", path.node.name);
-					} else {
-						console.log("🤖 Identifier %s: %s", path.node.name, path.parentPath?.node?.type);
-					}
-				},
-			},
-			MemberExpression: {
-				exit: (path: NodePath<MemberExpression>) => {
-					if (path.node.computed) return
-					const object = path.node.object as Identifier | Expression;
-					const property = path.node.property as Identifier | Expression;
-					if (property.type === "Identifier") {
-						console.log("🤖 MemberExpression fetch dependency %s", property.name);
-					}
-				}
-			},
-			BinaryExpression: {
-				exit: (path: NodePath<BinaryExpression>) => {
-					console.log("🤖 BinaryExpression: ", path.node.operator);
-					const operatorMap: Record<string, Opcode> = {
-						"+": Opcode.Add,
-						"-": Opcode.Sub,
-						"*": Opcode.Mul,
-						"/": Opcode.Div,
-					};
-					const operator = operatorMap[path.node.operator];
-					if (operator) {
-						this.pushIr(createInstruction(operator));
-					} else {
-						throw new Error(`🤖 Unknown operator: ${path.node.operator}`);
-					}
-				},
-			},
-			NumericLiteral: {
-				exit: (path: NodePath<NumericLiteral>) => {
-					console.log("🤖 NumericLiteral: ", path.node.value);
-					this.pushIr(
-						createInstruction(Opcode.Push, [
-							{ kind: ArgKind.Number, value: path.node.value },
-						]),
-					);
-				},
-			},
-		};
+	private compileStatement(node: Statement) {
+		switch (node.type) {
+			case "ExpressionStatement":
+				console.log("🤖 Compiling VariableDeclaration");
+				break;
+			default:
+				throw new Error(`Unsupported statement type: ${node.type}`);
+		}
 	}
 
 	private pushIr(instruction: Instruction) {
